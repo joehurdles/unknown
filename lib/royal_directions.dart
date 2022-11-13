53ad6aa85a5a4e4b75f4b2565e3e29e48cb062ba
joehurdles <joeenocent@gmail.com>
2022-11-13 08:14:01 +0000

    new files

--- lib/royal_directions.dart
+++ lib/royal_directions.dart
@@ -14,6 +14,7 @@ class Directions43 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
           body: Column(
--- lib/wenya_directions.dart
+++ lib/wenya_directions.dart
@@ -14,6 +14,7 @@ class Directions29 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
           body: Column(
